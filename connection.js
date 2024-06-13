499810fcf6c7b75f9e7453409597ca91fe68241c
Campionii <bruno.olv.campioni@gmail.com>
2024-06-13 16:55:53 -0300

    commit

---
+++ connection.js
@@ -0,0 +1,21 @@
+// connection.js
+
+/*
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZBkPirRQoGoQs_xSkOEfANqvUgcP1ueA",
+  authDomain: "conectionstore-23778.firebaseapp.com",
+  projectId: "conectionstore-23778",
+  storageBucket: "conectionstore-23778.appspot.com",
+  messagingSenderId: "355682776468",
+  appId: "1:355682776468:web:a888cb4d3f950ab4db4b81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const bancoExterno = getFirestore(app);
+
+export { bancoExterno };*/
